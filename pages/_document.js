2b92399454e7588fb549e06f5f425b832886db28
Simon Hudson <hellosimonhudson+github@gmail.com>
2019-01-25 09:56:56 +0000

    UI work

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,7 @@ export default class App extends Document {
 			<Head>
 				<title></title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet" /> 
 				<link rel="stylesheet" href="/_next/static/style.css" />
 			</Head>
 			<body>
